81520f002b000590bda7f3da1db7ff51fb1c5647
Patataman917 <133590918+Patataman917@users.noreply.github.com>
2024-12-21 11:37:33 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+﻿self.addEventListener('install', event => {
+    console.log('Service Worker instalado');
+    event.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll(['./']);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activado');
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: data.icon
+    });
+});
